d1b104a04a99f36dd4d18872389a0d3b17454646
HellerDeveller <150496986+HellerDeveller@users.noreply.github.com>
2024-11-20 18:57:28 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class TestSum(unittest.TestCase):
         data = [1, 2, 3]
         result = sum(data)
         self.assertEqual(result, 6)
-
+        
     def test_list_fraction(self):
         data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
         result = sum(data)
@@ -16,3 +16,6 @@ class TestSum(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+# at top indicates number of tests, 'F' means one failed, '.' means one passed
+# names the tests in (__main__.Class.Function)
